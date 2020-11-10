54a398ff0631bc7be8ddd760cff5919cd173866f
Alex-2kZharkov <Alexander.06.zharkov@gmail.com>
2020-11-10 14:34:39 +0600

    Staring todo-list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './index';
 
+const addValue = (values, value) => {
+  values.push({ value });
+  localStorage.setItem('values', JSON.stringify(values));
+};
 function App() {
+  let currentCounter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  window.addEventListener('load', (event) => {
+    console.log(JSON.parse(localStorage.getItem('values')));
+  });
+
+  let values = JSON.parse(localStorage.getItem('values'));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Current count {currentCounter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => addValue(values, currentCounter)}>
+        Save to local storage
+      </button>
     </div>
   );
 }
