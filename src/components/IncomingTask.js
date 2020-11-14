d64b10d63552d0539ca8ee91b24427235297d705
Alex-2kZharkov <Alexander.06.zharkov@gmail.com>
2020-11-14 19:17:22 +0600

    Removing animation

--- src/components/IncomingTask.js
+++ src/components/IncomingTask.js
@@ -6,22 +6,19 @@ import { countMargin } from '../actions/countOffset';
 
 const IncomingTask = (props) => {
 
-  const [lineThroughClass, changeLineThroughClass] = useState(''), [riseShadowClass, changeRiseShadow] = useState(''), [hiddenTask, changeHiddenTask] = useState({});
+  const [lineThroughClass, changeLineThroughClass] = useState(''), [riseShadowClass, changeRiseShadow] = useState(''), 
+  [hiddenTask, changeHiddenTask] = useState({}), [removingAnimation, changeRemovingAnimation] = useState('');
   let transformedLocation, offsetY = 0, fulfilledTaskOffset = useSelector(state => state.offset);
   const taskRef = useRef(null);
   const  dispatch = useDispatch();
 
-  const showRef = () => {
-    
+  const markAsFulfilled = () => {
     let taskRefCopy = taskRef.current;
 
     while(taskRefCopy) {
       offsetY += taskRefCopy.offsetTop;
       taskRefCopy = taskRefCopy.offsetParent;
   }
-  }
-
-  const markAsFulfilled = () => {
     changeLineThroughClass('line_through_text');
     changeRiseShadow('flash_border');
 
@@ -35,6 +32,11 @@ const IncomingTask = (props) => {
     }
     setTimeout(changeStatusLocation, 2000);
   }
+
+  const onRemoveClick = () => {
+    changeRemovingAnimation('remove_task_anim');
+  }
+
   if (props.location.includes(' ') ) {
     let lastSpace = props.location.lastIndexOf(' ');
     transformedLocation = `${props.location.slice(0, lastSpace)} ,${props.location.slice(lastSpace)}`; // inserts comma
@@ -44,24 +46,24 @@ const IncomingTask = (props) => {
   let date = new Date(year, month - 1, day).toLocaleDateString('ru', { year: 'numeric', month: 'long', day: 'numeric'});
 
   return (
-    <div className='task' style={hiddenTask} ref={taskRef} onClick={showRef}>
+    <div className={`task ${removingAnimation}`} style={hiddenTask} ref={taskRef} >
       <div className={`task-icon-container ${riseShadowClass}`}>
         <img src={process.env.PUBLIC_URL + `/images/${props.category}_darker.jpg`} className='task-icon' alt='icon' />
       </div>
       <div className='task_information'>
-        <p className={`task-text ${lineThroughClass}`} onClick={markAsFulfilled}>
+        <p className={`task-text ${lineThroughClass}`} >
           {props.text}
         </p>
         <div className='task-location'>{transformedLocation}</div>
       </div>
       <div className='task_date_created'>
         <div className='task_date'>{date}</div>
-        <div className='task_time'>19:10</div>
+        <div className='task_time'>{props.date[1]}</div>
       </div>
       <div className='control_buttons'>
-        <button className='control_button edit_button_container'></button>
-        <button className='control_button delete_button_container'></button>
-        <button className='control_button done_button_container'></button>
+        <button className='control_button edit_button_container' ></button>
+        <button className='control_button delete_button_container' onClick={onRemoveClick}></button>
+        <button className='control_button done_button_container' onClick={markAsFulfilled}></button>
       </div>
     </div>
   );
